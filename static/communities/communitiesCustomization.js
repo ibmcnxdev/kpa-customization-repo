cee416385df1f660e0a79aeb2a2fa1fa65f32c8f
Tuukka Lindroos <tuukka.lindroos@gmail.com>
2018-01-10 17:27:24 +0200

    initial

---
+++ static/communities/communitiesCustomization.js
@@ -0,0 +1,12 @@
+////
+// @author Tuukka Lindroos, KPA Unicon
+// @name Communities Customization
+// @version 1.0
+//
+if(typeof(dojo) != "undefined") {
+    dojo.place(
+        "<link rel=\"stylesheet\" type=\"text/css\" href=\"/files/muse-static/communities/communitiesCustomization.css\"></link>",
+        dojo.doc.head,
+        "last"
+    );
+}
